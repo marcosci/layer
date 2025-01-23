7608b43c435d0ee383b038a98626883f1b3d1ab7
Egor Kotov <kotov.egor@gmail.com>
2025-01-23 21:05:16 +0100

    add package citation for data

--- R/data.R
+++ R/data.R
@@ -1,35 +1,31 @@
-#' Landscape 1.
+#' Landscape 1
 #'
-#' Random curd neutral landscape model.
+#' Random curd neutral landscape model generated using the `NLMR` package.
 #'
-#' @format raster object
-#'
-#' @source NLMR package
+#' @format A \code{\link[raster]{raster}} object.
+#' @source Data generated using the \code{\link[NLMR]{NLMR}} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
 "landscape_1"
 
-#' Landscape 2.
+#' Landscape 2
 #'
-#' Fractional Brownian motion neutral landscape model.
+#' Fractional Brownian motion neutral landscape model generated using the `NLMR` package.
 #'
-#' @format raster object
-#'
-#' @source NLMR package
+#' @format A \code{\link[raster]{raster}} object.
+#' @source Data generated using the \code{\link[NLMR]{NLMR}} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
 "landscape_2"
 
-#' Landscape 3.
+#' Landscape 3
 #'
-#' Distance gradient neutral landscape model.
+#' Distance gradient neutral landscape model generated using the `NLMR` package.
 #'
-#' @format raster object
-#'
-#' @source NLMR package
+#' @format A \code{\link[raster]{raster}} object.
+#' @source Data generated using the \code{\link[NLMR]{NLMR}} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
 "landscape_3"
 
-#' Landscape Points.
+#' Landscape Points
 #'
-#' Random curd neutral landscape model.
+#' Random curd neutral landscape model generated using the `NLMR` package.
 #'
-#' @format raster object
-#'
-#' @source NLMR package
+#' @format A \code{\link[raster]{raster}} object.
+#' @source Data generated using the \code{\link[NLMR]{NLMR}} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
 "landscape_points"
