f86b08a25a6c4ec9772196a8662237dc950af9fe
Egor Kotov <kotov.egor@gmail.com>
2025-01-23 21:14:26 +0100

    link to NLMR package website instead of package link as it is removed from CRAN

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' Random curd neutral landscape model generated using the `NLMR` package.
 #'
 #' @format A \code{\link[raster]{raster}} object.
-#' @source Data generated using the \code{\link[NLMR]{NLMR}} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
+#' @source Data generated using the \href{https://ropensci.github.io/NLMR/}{NLMR} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
 "landscape_1"
 
 #' Landscape 2
@@ -11,7 +11,7 @@
 #' Fractional Brownian motion neutral landscape model generated using the `NLMR` package.
 #'
 #' @format A \code{\link[raster]{raster}} object.
-#' @source Data generated using the \code{\link[NLMR]{NLMR}} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
+#' @source Data generated using the \href{https://ropensci.github.io/NLMR/}{NLMR} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
 "landscape_2"
 
 #' Landscape 3
@@ -19,7 +19,7 @@
 #' Distance gradient neutral landscape model generated using the `NLMR` package.
 #'
 #' @format A \code{\link[raster]{raster}} object.
-#' @source Data generated using the \code{\link[NLMR]{NLMR}} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
+#' @source Data generated using the \href{https://ropensci.github.io/NLMR/}{NLMR} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
 "landscape_3"
 
 #' Landscape Points
@@ -27,5 +27,5 @@
 #' Random curd neutral landscape model generated using the `NLMR` package.
 #'
 #' @format A \code{\link[raster]{raster}} object.
-#' @source Data generated using the \code{\link[NLMR]{NLMR}} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
+#' @source Data generated using the \href{https://ropensci.github.io/NLMR/}{NLMR} package. See Sciaini et al. (2018) for details: \doi{10.1111/2041-210X.13076}.
 "landscape_points"
